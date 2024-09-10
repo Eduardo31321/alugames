15ee8224d5e1c1f6127746ff3bebd9cfc60f02c6
Eduardo <narutuuo3132@gmail.com>
2024-09-10 19:58:30 +0900

    AluraGames

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function alterarStatus (id) {
+    let gameClicando = document.getElementById(`game-${id}`);
+    let imagem = gameClicando.querySelector('.dashboard__item__img');
+    let botao = gameClicando.querySelector('.dashboard__item__button');
+    let nomedoJogo = gameClicando.querySelector('.dashboard__item__name');
+    let jogosAlugados = 0;
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomedoJogo.textContent}?`)) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        jogosAlugados--;
+        }
+    } else {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+            jogosAlugados++;
+        }
+
+    contarEExibirJogosAlugados();
+}
+
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
+    
